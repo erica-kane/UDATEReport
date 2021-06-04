a3d33e5ef75f24bbb9eb94c63810e235193ab4b8
ericamcg <71267518+ericamcg@users.noreply.github.com>
2021-06-04 17:37:07 +0100

    Update reportscript.py

--- reportscript.py
+++ reportscript.py
@@ -1,12 +1,15 @@
+# Import packages and libraries 
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+from datetime import datetime
 
 # Read in the data sets
 store = pd.read_csv('store.csv')
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
+
 
 # Check all store values are unique and the same amount are in each data set 
 pd.isnull(store).sum()
@@ -14,18 +17,28 @@ store['Store'].nunique()
 pd.isnull(train).sum()
 train['Store'].nunique()
 
+
 # Join store and train on store number 
 full = train.merge(store,  on='Store')
+
 
 # Check sales are 0 when the stores are closed 
 closed = full[full['Open'] == 0]
 (closed['Sales'] == 0).all()
-
+# Check for negative sales values 
+(full['Sales'] < 0 ).any()
 # Check sales for outliers 
 full['Sales'].describe()
 plt.hist(full['Sales'], bins=20)
 # The highest sales and largest amount of customers frequently lie in store 262 - may not be outliers just a large busy store 
-full[full['Sales'] > 38000]
+full[full['Sales'] > 35000]
+# Store 909 has highest sales - look at the distribution of its other sales to see if it is fitting
+(full['Sales'][full['Store'] == 909]).describe()
+(full['Sales'][full['Store'] == 57]).describe()
+(full['Sales'][full['Store'] == 817]).describe()
+full = full.drop([full.index[51506] , full.index[827591]])
+plt.hist(full['Sales'], bins=20)
+
 
 # Check all dates are within the years 
 full['Date'] = pd.to_datetime(full['Date'], dayfirst=True)
@@ -33,13 +46,12 @@ full['Date'] = pd.to_datetime(full['Date'], dayfirst=True)
 full['Date'].dt.month
 str(full['Date'].dt.date.min()) + ' to ' + str(full['Date'].dt.date.max())
 
-# Check for negative sales values 
-(full['Sales'] < 0 ).any()
 
 # Check all 0 and 1 values are 0 and 1 
 for column in ['Open', 'Promo', 'SchoolHoliday', 'Promo2']:
     print(column)
     print(full[column].unique())
+
 
 # Check all abcd variables are correct 
 for column in ['StoreType', 'StateHoliday', 'Assortment']:
@@ -49,32 +61,48 @@ full = full.replace({'StateHoliday' : { 'a' : 'Public holiday',
 'b' : 'Easter holiday', 'c' : 'Christmas', 0 : 'None', '0' : 'None'}})
 full = full.replace({'Assortment' : { 'a' : 'Basic', 'b' : 'Extra', 'c' : 'Extended'}})
 
+
 # Check values of competition since month 
 for column in ['CompetitionOpenSinceMonth', 'CompetitionOpenSinceYear', 'Promo2SinceWeek', 'Promo2SinceYear']:
     print(column)
     print(full[column].unique())
-    
+# 1990 is major outlier in CompSinceYear, see how many rows this is the case and for what store 
+(full['Store'][full['CompetitionOpenSinceYear'] == 1900]).unique()
+# Recode to 1995 
+for index, value in full['CompetitionOpenSinceYear'].iteritems():
+    if value == 1900:
+        full['CompetitionOpenSinceYear'][index] = 1995
+
+
 # Check distribution of competition distance for outliers 
 full['CompetitionDistance'].describe()
 plt.hist(full['CompetitionDistance'], bins=20)
-full[full['CompetitionDistance'] > 55000]
+(full['Store'][full['CompetitionDistance'] > 55000]).unique()
+
 
 # Drop customers
 full = full.drop(columns=['Customers'])
+
 
 # Recode day of week to strings 
 full = full.replace({'DayOfWeek' : { 1 : 'Monday', 2 : 'Tuesday', 3 : 'Wednesday',
 4 : 'Thursday', 5 : 'Friday', 6 : 'Saturday', 7 : 'Sunday'}})
 full['DayOfWeek'].unique()
 
+
 # Deal with holiday variables (drop state holidays because it's really just a proxy for open/closed)
-full = full.drop(columns=['StateHoliday', 'SchoolHoliday'])
+holiday = full[full['StateHoliday'] != 'None']
+holiday[holiday['Open'] == 1]
+#full = full.drop(columns=['StateHoliday'])
+
 
 # Add variables 
 full['Month'] = pd.DatetimeIndex(full['Date']).month
 full['Year'] = pd.DatetimeIndex(full['Date']).year
 full['Week'] = full['Date'].dt.week
 
+
+# Create DaysSinceComp
 # Combine competition month and year 
 # Deal with inconsistencies in Year and Month columns 
 full['CompetitionOpenSinceYear'].unique()
@@ -84,46 +112,44 @@ datedf = full[['CompetitionOpenSinceYear', 'CompetitionOpenSinceMonth']]
 datedf = datedf.rename(columns={"CompetitionOpenSinceYear": "year", "CompetitionOpenSinceMonth": "month"})
 datedf['day'] = 1
 full['CompDate'] = pd.to_datetime(datedf)
-# Create months since variable 
-full['MonthsSinceComp'] = ((full.Date - full.CompDate)/np.timedelta64(1, 'M'))
-full['MonthsSinceComp'] = full['MonthsSinceComp'].round()
+# Create days since variable 
+def get_days(df):
+    date = df['Date']
+    compdate = df['CompDate']
+    if pd.notnull(compdate):
+        day = (date - compdate).days
+        return day
+    else:
+        return np.NaN
+full['DaysSinceComp'] = full.apply(get_days, axis=1)
 # Check for negative values and how many there are 
-dates = []
-for value in full['MonthsSinceComp']:
-    if value < 0:
-        dates.append(value)
-len(dates)
+(full['DaysSinceComp'] <0).sum()
 # Change negative values to 0 
-for index, value in full['MonthsSinceComp'].iteritems():
+for index, value in full['DaysSinceComp'].iteritems():
     if value < 0:
-        full['MonthsSinceComp'][index] = 0
-(full['MonthsSinceComp'] < 0 ).any()
+        full['DaysSinceComp'][index] = 20000
+(full['DaysSinceComp'] > 20000 ).any()
+(full['DaysSinceComp'] == 20000 ).sum()
 
-# Look at realtionship between MonthsSinceComp and Sales and see if there is a need to move furhter with the variable 
-plt.scatter(full['Sales'], full['MonthsSinceComp'], s=1)
-full['MonthsSinceComp'].corr(full['Sales'])
-# No point continuing, there is very little realtion between the 2 variables
 
-# Use the MonthsSince variable to change distance to 0 if the comp isn't open yet 
-for index, value in full['MonthsSinceComp'].iteritems():
-    if value == 0:
-        full['CompetitionDistance'][index] = 0 
+# Use the DaysSince variable to change distance to 0 if the comp isn't open yet 
+for index, value in full['DaysSinceComp'].iteritems():
+    if value == 20000:
+        full['CompetitionDistance'][index] = 78000 
+(full['CompetitionDistance'] == 78000 ).sum()
 
+
+# Create PromoLength
 # Check that all values which are missing in PromoSinceWeek and PromoSinceYear and 0 in Promo2
 full['Promo2SinceWeek'].isnull().sum()
 full['Promo2SinceYear'].isnull().sum()
-
 for index, value in full['Promo2SinceWeek'].iteritems():
     if (np.isnan(value)) == True and full['Promo2'][index] != 0:
         print(value)
-
 # Combine PromoSinceWeek and PromoSinceYear to date varibale 
 full['Promo2SinceWeek'] = full['Promo2SinceWeek'].astype("Int64")
 full['Promo2SinceYear'] = full['Promo2SinceYear'].astype("Int64")
-
-full['Promo2SinceYear']
-from datetime import datetime
-
+# Build funciton which combines week of year and year into datetime object 
 def week_of_year_to_datetime(df):
     year = df['Promo2SinceYear']
     week = df['Promo2SinceWeek']
@@ -131,15 +157,88 @@ def week_of_year_to_datetime(df):
         datestring = str(year) + " " + str(week) + " 0"
         return datetime.strptime(datestring, "%Y %W %w")
     else:
-        return pd.NA
-
+        return np.NaN
 full['PromoSince'] = full.apply(week_of_year_to_datetime, axis=1)
+# Remove the time from the datetime
 full['PromoSince'] = pd.to_datetime(full['PromoSince']).dt.date
 full['PromoSince'] = pd.to_datetime(full['PromoSince'])
-
-# Create PromoLength 
-#full['PromoLength'] = ((full.Date - full.PromoSince)/np.timedelta64(1, 'M')).round()
-(full['PromoLength'] < 0 ).sum()
-
+# Create PromoLength - days since store started participating in Promo2 
 full['PromoLength'] = (full.Date - full.PromoSince)
+for index, value in full['PromoLength']:
+    if pd.notnull(value):
+        full['PromoLength'][index] = value.days
+# Extract just the days from the datetime 
+def get_days(df):
+    timedays = df['PromoLength']
+    if pd.notnull(timedays):
+        days = timedays.days
+        return days 
+    else:
+        return np.NaN
+full['PromoLength'] = full.apply(get_days, axis=1)
+# Observe how many negative values there are (promotion started after current date)
+(full['PromoLength'] < 0 ).sum()
+for index, value in full['PromoLength'].iteritems():
+    if pd.notnull(value) and value < 0:
+        full['PromoLength'][index] = -1
+(full['PromoLength'] == -1 ).sum()
+full['PromoLength'] = full['PromoLength'].astype('Int64')
 
+
+# Relitivise PromoInterval to current day 
+full['PromoInterval'].isnull().sum()
+def split_months(df):
+    months = df['PromoInterval']
+    if pd.notnull(months):
+        monlist = months.split(",")
+        return monlist 
+    else:
+        return np.NaN
+full['PromoInterval'] = full.apply(split_months, axis=1)
+sample2 = full.sample(10000)
+def give_sincecoupon(df):
+    dates = df['PromoInterval']
+    years = df['Year']
+    currentdate = df['Date']
+    if isinstance(dates, list):
+        datelist = []
+        datedifferences = []
+        for value in dates:
+            value = value.replace("Sept", "Sep")
+            date = '01' + value + str(years)
+            datelist.append(pd.to_datetime(datetime.strptime(date, '%d%b%Y')))
+        for value in datelist:
+            difference = (currentdate - value)
+            if difference.days >= 0:
+                datedifferences.append(difference.days)
+        if not datedifferences:
+            minval = -1 
+        else:
+            minval = min(datedifferences)
+        return minval
+    else:
+        return np.NaN
+full['SinceCoupon'] = full.apply(give_sincecoupon, axis=1)
+
+
+# Relaitionships between variables and Sales 
+# DaysSinceComp and Sales and see if there is a need to move furhter with the variable 
+plt.scatter(full['Sales'], full['DaysSinceComp'], s=1)
+full['DaysSinceComp'].corr(full['Sales'])
+# No point continuing, there is very little realtion between the 2 variables
+
+# Look at realtionship between PromoLength and Sales and see if there is a need to move furhter with the variable 
+sample = full.sample(10000)
+plt.scatter(sample['PromoLength'].astype('float'), sample['Sales'].astype('float'), s=1)
+full['PromoLength'].astype('float').corr(full['Sales'].astype('float'))
+# No point continuing, there is very little realtion between the 2 variables
+
+
+# Deal with missing values (of those which realte to sales)
+full.isnull().sum()
+
+
+# Creating the dataset 
+
+
+# Model
